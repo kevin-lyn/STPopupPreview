897f570530f0e8f7bd87684114dfcbc0a665b708
Kevin <kevin_lyn@outlook.com>
2016-05-22 12:26:42 +0800

    Initial Commit

---
+++ STPopupPreview/STPopupPreview.h
@@ -0,0 +1,19 @@
+//
+//  STPopupPreview.h
+//  STPopupPreview
+//
+//  Created by Kevin Lin on 22/5/16.
+//  Copyright © 2016 Sth4Me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STPopupPreview.
+FOUNDATION_EXPORT double STPopupPreviewVersionNumber;
+
+//! Project version string for STPopupPreview.
+FOUNDATION_EXPORT const unsigned char STPopupPreviewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STPopupPreview/PublicHeader.h>
+
+
